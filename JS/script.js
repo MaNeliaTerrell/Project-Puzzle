5219cd65ec76086a8495717b68c674ccfbd0dec3
MaNeliaTerrell <nterrell1014@gmail.com>
2022-09-19 14:47:13 -0400

    first commit

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+
+
+
+const hpImages = [{image:src="./"}
+                    // 'Images/Nagini.jpg',
+                    // 'Images/Pendant Horcrux.jpg',
+                    // 'Images/Hufflepuff\'s cup.jpg',
+                    // 'Images/Voldemort\'s ring.jpg',
+                    // 'Images/Tom M Riddle diary.jpg',
+                    // 'Images/Ravenclaw Diadem.jpg',
+]
+// console.log(hpImages);
+
+// const buttons = document.querySelector(".buttons")
+// const draw = document.querySelector("draw")
+// const reset = document.querySelector("#reset")
+
+// const drawCard = () => {
+//     let randomCard = hpImages[(Math.floor(Math.random() * hpImages.length))]
+//     const img = document.createElement('img')
+//     img.src = randomCard.image
+//     setOfCards.appendChild(img)
+// }
+// // draw.addEventListener('click', drawCard)
+
+// const setOfCards = document.querySelector("#set-of-cards")
+
